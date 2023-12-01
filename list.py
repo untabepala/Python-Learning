b9380203867d1342cbb15bce9fd111309cbb55f0
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2023-12-02 04:52:18 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+x=[5,6,7,8]
+y=['a','b','c','d']
+z=[5,3,5,'c','python',True,[1,2,3,]]
+print(z[4])
+x[1]=23
+print(x[1])
+len(x)
+print(len(x))
+x.insert(2,12)
+print(x)
+x.remove(7)
+print(x)
+x.pop()
+print(x)
+del x
+print(x)
