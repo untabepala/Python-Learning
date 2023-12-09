6923157ea9f1ac982d50bc919b230e32d71ea0a6
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2023-12-09 09:20:53 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+print(4+8)
+#normal bedima
+print(7/2)
+
+#purna sankywk gnn bedimak ntuw
+print(7//2)
+
+#balaya
+print(4**3)
+
+y=input('enter')
+print('name is '+y)
+x=float(input('enter flot'))
+print(x)
